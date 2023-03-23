fc452e90e1045225508557124ade33305d3bfd85
Levi Santos <levi.santos@suporterenova.onmicrosoft.com>
2023-03-23 13:50:16 -0300

    deletando pastas não utilizadas

--- az204-blob/obj/Release/net6.0/az204-blob.AssemblyInfo.cs
+++ az204-blob/obj/Release/net6.0/az204-blob.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("teste2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("az204-blob")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("teste2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("teste2")]
+[assembly: System.Reflection.AssemblyProductAttribute("az204-blob")]
+[assembly: System.Reflection.AssemblyTitleAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
