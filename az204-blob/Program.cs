99927aa089a329a73bba9decaa71995a6d6f24f5
Levi Santos <levi.santos@suporterenova.onmicrosoft.com>
2023-03-23 13:40:13 -0300

    adicionando blobs no container

---
+++ az204-blob/Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+
+Console.WriteLine("Aprendendo a mandar arquivos pro blob\n");
+
+// Run the examples asynchronously, wait for the results before proceeding
+ProcessAsync().GetAwaiter().GetResult();
+
+Console.WriteLine("Press enter to exit the sample application.");
+Console.ReadLine();
+
+static async Task ProcessAsync()
+{
+ 
+    string storageConnectionString = "connectionString";
+    var blobStorageContainerName = "testecriandocontainerpelocodigo";
+
+    var container = new BlobContainerClient(storageConnectionString, blobStorageContainerName);
+
+  
+    var files = Directory.GetFiles(@"C:\Users\levi.santos\blob");
+    foreach (var file in files)
+    {
+        using (FileStream uploadFileStream = File.OpenRead(file))
+        {
+            BlobClient blobClient = container.GetBlobClient(file);
+            await blobClient.UploadAsync(uploadFileStream);
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+            uploadFileStream.Close();
+        }
+        Console.WriteLine("O arquivo foi updado{0}",file);
+    }
+    foreach(var file in files)
+    { //deleta os arquivos da pagina
+        File.Delete(file);
+        Console.WriteLine("O arquivo {0} foi deletado",file);
+    }
+    Console.WriteLine("\nThe file was uploaded. We'll verify by listing" +
+            " the blobs next.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+}
---
+++ teste2/Program.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+Console.WriteLine("Azure Blob Storage exercise\n");
+
+// Run the examples asynchronously, wait for the results before proceeding
+ProcessAsync().GetAwaiter().GetResult();
+
+Console.WriteLine("Press enter to exit the sample application.");
+Console.ReadLine();
+
+static async Task ProcessAsync()
+{
+    // Copy the connection string from the portal in the variable below.
+    string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=rinspection890b;AccountKey=NXkwEMVztcXfHMuASaoG8sdBl7gQY0b9PJNN+uyaSl0jBr8nAbBB6/ZwcBJnkuA8/THIOwXMFfW6+AStqOp8rQ==;EndpointSuffix=core.windows.net";
+
+    // Create a client that can authenticate with a connection string
+    BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+    string containerName = "wtblob" + Guid.NewGuid().ToString();
+
+    // Create the container and return a container client object
+    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+    Console.WriteLine("A container named '" + containerName + "' has been created. " +
+        "\nTake a minute and verify in the portal." +
+        "\nNext a file will be created and uploaded to the container.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+    string localPath = "./data/";
+    string fileName = "wtfile" + Guid.NewGuid().ToString() + ".txt";
+    string localFilePath = Path.Combine(localPath, fileName);
+
+    // Write text to the file
+    await File.WriteAllTextAsync(localFilePath, "Hello, World!");
+
+    // Get a reference to the blob
+    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+    Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+    // Open the file and upload its data
+    using (FileStream uploadFileStream = File.OpenRead(localFilePath))
+    {
+        await blobClient.UploadAsync(uploadFileStream);
+        uploadFileStream.Close();
+    }
+
+    Console.WriteLine("\nThe file was uploaded. We'll verify by listing" +
+            " the blobs next.");
+    Console.WriteLine("Press 'Enter' to continue.");
+    Console.ReadLine();
+
+}
